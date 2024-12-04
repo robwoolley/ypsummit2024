e58408e0cab09e7e7a91236270ea7d465688586e
Rob Woolley <rob.woolley@windriver.com>
2024-12-04 13:26:46 +0000

    Add an infinite loop that can be enabled.
    Signed-off-by: Rob Woolley <rob.woolley@windriver.com>

--- src/main.c
+++ src/main.c
@@ -1,10 +1,29 @@
 #include "mylib.h"
+#include "stdio.h"
+#include "unistd.h"
+
+int loop() {
+    char c;
+    printf("Press 'q' to quit the infinite loop.\n");
+
+     while(1) {
+	c = getc(stdin);
+	if (c == 'q') {
+            break;
+        }
+
+        hello();
+        sleep(1);
+    }
+
+    int n = 3; // Number of disks
+    hanoi(n, 'A', 'C', 'B');
+
+    return 0;
+}
 
 int main() {
     hello();
-
-    // int n = 3; // Number of disks
-    // hanoi(n, 'A', 'C', 'B');
 
     return 0;
 }
